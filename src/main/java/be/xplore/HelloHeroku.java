bfa07ca57708564bbe80b763ee95c917f7307fc2
Jens De Temmerman <jens@jensdt.be>
2017-12-10 13:50:05 +0100

    Initial commit

--- src/main/java/be/xplore/HelloHeroku.java
+++ src/main/java/be/xplore/HelloHeroku.java
@@ -6,8 +6,9 @@ import io.undertow.util.Headers;
 public class HelloHeroku {
 
     public static void main(String[] args) {
+
         Undertow server = Undertow.builder()
-                .addHttpListener(8080, "0.0.0.0")
+                .addHttpListener(port(), "0.0.0.0")
                 .setHandler(exchange -> {
                     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
                     exchange.getResponseSender().send("Hello World");
@@ -16,5 +17,15 @@ public class HelloHeroku {
         server.start();
 
         System.out.println("Started server at http://127.1:8080/  Hit ^C to stop");
+    }
+
+    private static int port() {
+        String envPort = System.getenv("PORT");
+
+        if (envPort == null) {
+            return 8080;
+        } else {
+            return Integer.parseInt(envPort);
+        }
     }
 }
